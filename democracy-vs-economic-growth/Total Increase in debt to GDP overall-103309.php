8430d31f7cd9783c793bf41610c943b9a53bae92
Mike P. Sinn <m@quantimodo.com>
2021-08-10 00:52:55 -0500

    Images

---
+++ democracy-vs-economic-growth/Total Increase in debt to GDP overall-103309.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" id="sixapart-standard">
+<body id="uploaded-image-popup" class="popup" style="margin: 0;padding: 0;">
+<div id="popup-container">
+    <div id="popup-container">
+        <img src="http://cdn.theatlantic.com/static/mt/assets/steve_clemons/Total%20Increase%20in%20debt%20to%20GDP%20overall.jpg" width="540" height="443" style="margin: 0;padding: 0;border: 0;" />
+    </div>
+</div>
+</body>
+</html>
